a2304ccd660a48ade53a938f68bfe43fb53dc9e1
Singhmann459 <anmolsinghmann20@gmail.com>
2023-12-15 20:38:04 -0500

    redoing the css and fixing paths

--- index.php
+++ index.php
@@ -8,11 +8,10 @@ if (!isset($_SESSION["user"])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="C:\xampp\htdocs\Restourant\WebSite\style1.css">
-    <title>User Dashboard</title>
+    <link rel="stylesheet" href="style1.css">
+    <title>Restaurant</title>
 </head>
 <body class="">
     
@@ -20,7 +19,7 @@ if (!isset($_SESSION["user"])) {
         <header>
             <h1>Foodies</h1>
             <div class="shopping">
-                <img src="image/shopping.svg">
+                <img src="shopping.svg">
                 <span class="quantity">0</span>
             </div>
         </header>
@@ -41,5 +40,4 @@ if (!isset($_SESSION["user"])) {
     <a href="logout.php" class="btn btn-warning">Logout</a>
 
     <script src="app.js"></script>
-</body>
 </html>
